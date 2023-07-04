d72a93f7ed285e4de84395767feee5449f12ea61
Aditya Yaduvanshi <aditya97y@gmail.com>
2023-07-04 13:41:31 +0530

    fix

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -2,7 +2,7 @@ import { Billboard } from '@/types';
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboards = async (id: string): Promise<Billboard[]> => {
+const getBillboards = async (id: string): Promise<Billboard> => {
   const res = await fetch(`${URL}/${id}`);
 
   return res.json();
